8231a1e8331c1a247c770575c4cd6ef0004b2bcb
GodBell <12191579@inha.edu>
2021-06-06 05:08:47 +0900

    20210606 0508 include db.js

--- src/db.js
+++ src/db.js
@@ -1,5 +1,5 @@
 /*
- * 파일명: db_template.js
+ * 파일명: db.js
  * 생성일: 2021-02-08
  * 최종 수정일: 2021-06-06
  * 설명: Web 백엔드 서버 개발 공부: DB 연결정보 분리
@@ -9,13 +9,11 @@ import mysql from "mysql";
 
 let db_config_data = null;
 
-config();
-
 db_config_data = mysql.createPool({
-    host: '',
-    user: '',
-    password: '',
-    database: ''
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DB_NAME
 });
 
 export let db_config = db_config_data;
